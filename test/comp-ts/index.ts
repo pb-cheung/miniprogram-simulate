7c59ea1d3d2d10a85385926dafec7c02710a4360
pengbozhang <pengbozhang@tencent.com>
2021-12-23 16:13:48 +0800

    test(comp-ts): add ts test

---
+++ test/comp-ts/index.ts
@@ -0,0 +1,43 @@
+Component({
+    properties: {
+        hasChild: {
+            type: Boolean,
+            value: false,
+        },
+        prop: {
+            type: String,
+            value: 'index.properties',
+            observer(newVal, oldVal) {
+                this.setData({
+                    'observerArr[0]': newVal,
+                    'observerArr[1]': oldVal,
+                })
+            }
+        },
+    },
+    data: {
+        observerArr: [],
+    },
+    observers: {
+        'prop': function(value) {
+            this.setData({
+                'observerArr[2]': value,
+                'observerArr[3]': this.getStr(),
+            })
+        },
+    },
+    methods: {
+        getStr() {
+            return 'observers'
+        }
+    },
+    pageLifetimes: {
+        // @ts-ignore
+        show(args) {
+            this.setData({
+                'observerArr[4]': 'pageShow',
+                'observerArr[5]': args,
+            })
+        },
+    },
+})
